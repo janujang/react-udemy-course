536baa34584e278f932c65ab78b0008d3d9b2d4e
Janujan <janujan.g@hotmail.com>
2022-08-06 19:58:20 -0400

    add modal

---
+++ food-order-project/src/components/UI/Modal.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = () => {
+  return <></>;
+};
+
+export default Modal;
