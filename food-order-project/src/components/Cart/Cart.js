8e63cc57adc1890e58462f00491628588b596f96
Janujan <janujan.g@hotmail.com>
2022-08-06 08:51:25 -0400

    cart updates

--- food-order-project/src/components/Cart/Cart.js
+++ food-order-project/src/components/Cart/Cart.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import classes from "./Cart.module.css";
 const Cart = (props) => {
-  return <div>Cart</div>;
+  const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
+    (item) => <li>{item.name}</li>
+  );
+  return (
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Cart;
